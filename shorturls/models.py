04736cb896058e9f4dcfce7b055d6ab757bf6870
Holly Becker <becker.holly@gmail.com>
2019-06-20 15:07:32 -0700

    Add model to store URL mapping

--- shorturls/models.py
+++ shorturls/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
-# Create your models here.
+
+class URLMapping(models.Model):
+    """Stores mapping from original URL to shortened URL"""
+
+    # Most web browsers support URLs up to 2000 characters or so
+    original_url = models.CharField(
+        max_length=2000, help_text="URL to redirect to (max 2000 characters)"
+    )
+    short_url = models.CharField(
+        max_length=20, unique=True, help_text="Short URL string"
+    )
+
+    def __str__(self):
+        return f"{self.original_url} -> {self.short_url}"
